8582da9efd19a822177ba29f6cc6c3f8fc696212
ARTIOM\artio <artiomonilov2005@gmail.com>
2026-06-12 21:53:22 +0300

    New key

--- Configuration/OpenAIConfiguration.swift
+++ Configuration/OpenAIConfiguration.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum OpenAIConfiguration {
-    static let apiKey = "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"
+    static let apiKey = "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"
     static let visionModel = "gpt-5.5"
     static let chatCompletionsURL = URL(string: "https://api.openai.com/v1/chat/completions")!
 
